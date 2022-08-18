67fa1975b1573c4539ad060c7b3160f5d959c7fc
Howard Haryanto <hh@Howards-MacBook-Pro.local>
2022-08-18 12:27:53 -0500

    main.js - update to pull certain data from API, index.html - updated id's

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
-
-async function googleNews() {
-    const newsKey = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'de4299dbc8mshebf85f46aa18f61p113476jsne430c829d59e',
-            'X-RapidAPI-Host': 'google-news1.p.rapidapi.com'
-        }
-    };
-	const newsResponse = await fetch(`https://google-news1.p.rapidapi.com/top-headlines?country=US&lang=en&limit=10`, newsKey)
-	const newsData = await newsResponse.json();
-	console.log(newsData)
-}
-
-googleNews()
-
+// ***IGNORE*** $(document).ready(function() {
+// 	***IGNORE*** $("#submitButton").click(function() {
+		let countryCode = $("#countryCode").val();
+		async function getNews() {
+		const newsKey = {
+				method: 'GET',
+        		headers: {
+					'X-RapidAPI-Key': 'de4299dbc8mshebf85f46aa18f61p113476jsne430c829d59e',
+            		'X-RapidAPI-Host': 'google-news1.p.rapidapi.com'
+        			}
+    			};
+			const response = await fetch(`https://google-news1.p.rapidapi.com/top-headlines?country=${countryCode}&lang=en&limit=10`, newsKey);
+			const data = await response.json();
+			let newsArticles = Object.values(data.articles); // variable to return articles and their information
+			console.log(newsArticles);
+			}
+		getNews();
+	// ***IGNORE*** });
+// ***IGNORE*** });
 async function timeZone() {
     const timeKey = {
         method: 'GET',
@@ -24,7 +27,11 @@ async function timeZone() {
     };
 	const timeResponse = await fetch("https://weatherapi-com.p.rapidapi.com/timezone.json?q=London%20GB", timeKey)
 	const timeData = await timeResponse.json();
-	console.log(timeData)
+	//console.log(timeData);
+	// the below returns City, Country and local time
+	console.log(timeData.location.name);
+	console.log(timeData.location.country);
+	console.log(timeData.location.localtime);
 }
 
 timeZone()
@@ -42,7 +49,12 @@ async function weather() {
 
 	const weatherResponse = await fetch("https://weatherapi-com.p.rapidapi.com/current.json?q=London%3AGB", weatherKey)
 	const weatherData = await weatherResponse.json();
-	console.log(weatherData)
+	//console.log(weatherData)
+	// the below returns Celcius and Fahrenheit
+	console.log(weatherData.current.temp_c);
+	console.log(weatherData.current.temp_f);
+
+
 }
 
 weather()
